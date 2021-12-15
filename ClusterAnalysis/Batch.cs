73777fb7e01cd9adde836dd73656d7542e7b7c4e
Dave Tapson <dave.tapson@gmail.com>
2021-12-16 08:02:23 +1300

    Add project files.

---
+++ ClusterAnalysis/Batch.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+
+namespace ClusterAnalysis
+{
+    internal class Batch
+    {
+        internal void Run()
+        {
+            Log.Information("In run...");
+            Log.Information("About to do stuff...");
+
+            // stuff
+
+            // so, you need to find out how to import the data, and then hold it in a fashion that lets you investigate it.
+            // first prize is to save the files on your disk somewhere and then read them from there => file handling
+            // into a db => db handling
+            // and then save the results into db!
+
+            // once you have the data, you are looking for:
+            // ring1 with >= 4 positives (i.e. a positive value surrounded by >= 4 positive values)
+            // ring1 with >= 7 positives
+            // ring2 with >= 13 positives
+
+            // ideally you'd be looking to save the results of your investigation somehow, so that you can go back and see 
+            // what the results were as opposed to having to run the system again.
+
+            // I've just done it, let's see what you come up with :)
+            // Maybe I will learn something.
+
+            // I use BareTail to monitor logs - you run BareTail, point it at the log file and then it updates as the log changes.
+            // It doesn't lock the log file like notepad would do.  Notepad++ doesn't lock the file (which is why it's nice) but 
+            // it doesn't automatically update when the log changes.
+            // https://www.baremetalsoft.com/baretail/
+
+            // there are some data files in the DataFiles folder
+        }
+    }
+}
---
+++ ClusterAnalysis/Program.cs
@@ -0,0 +1,63 @@
+﻿using Serilog;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ClusterAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName projAssemblyName = GetAssemblyName();
+
+            SetupLogger(projAssemblyName.Name);
+
+            Log.Information($"{projAssemblyName.Name} {projAssemblyName.Version} started at {DateTime.Now}");
+
+            Console.WriteLine("Press ESC to stop.");
+
+            //Timer timer = new(Analyze, null, 0, 10000); // set timer to re-run in loop
+            Analyze(null);
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
+
+            Log.Information($"{projAssemblyName.Name} ended at {DateTime.Now}");
+            Log.CloseAndFlush();
+        }
+
+        private static void Analyze(Object o)
+        {
+            Batch _batch = new();
+
+            _batch.Run();
+
+            Console.Write("."); // when timer is running, this prints a dot to the console just so you know something is still happening
+        }
+
+        private static void SetupLogger(string projName)
+        {
+            // check folder exists
+            var folderPath = @"C:\Logs";
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            // set up logger
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(folderPath + @"\" + projName + ".txt", rollingInterval: RollingInterval.Month)
+                .CreateLogger();
+        }
+
+        private static AssemblyName GetAssemblyName()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            
+            var assemblyName = assembly.GetName();
+
+            return assemblyName;
+        }
+    }
+}
